ab2b8d8e4702cff692852febf482006459302231
prayogitio <tio.qynxx@gmail.com>
2018-03-05 12:51:22 +0700

    update port and Procfile to run heroku

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ app.use(express.static((path.join(__dirname, 'public'))));
 
 todoController(app);
 
-var server = app.listen(3000, function() {
-    console.log('you are listening on port 3000...');
+var server = app.listen(process.env.PORT || 8000, function() {
+    console.log('server online ...');
 });
 
 var io = socket(server);
